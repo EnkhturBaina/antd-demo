75e88d29a6e909b6864d54f47bc3608ebf23a9ad
enkhturBaina <eegii.111.gg@gmail.com>
2023-04-13 16:57:29 +0800

    zaswar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Button } from "antd";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Mayg3 from "./Mayg3";
 import Mayg4 from "./Mayg4";
-import СТ1_5 from "./СТ1_5";
+import СТ_15 from "./СТ_15";
 
 function App() {
   const [type, setType] = useState(1);
@@ -16,7 +16,7 @@ function App() {
       <Button onClick={() => setType(2)}>Mayg2</Button>
       <Button onClick={() => setType(3)}>Mayg3</Button>
       <Button onClick={() => setType(4)}>Mayg4</Button>
-      {type === 1 ? <СТ1_5 /> : null}
+      {type === 1 ? <СТ_15 /> : null}
       {type === 2 ? <Mayg2 /> : null}
       {type === 3 ? <Mayg3 /> : null}
       {type === 4 ? <Mayg4 /> : null}
