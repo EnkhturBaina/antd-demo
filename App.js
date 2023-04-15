480ace8f13714d62480b328838c42f6e9cc48e3a
EnkhturBaina <66661090+EnkhturBaina@users.noreply.github.com>
2023-04-15 20:58:07 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Mayg2 from "./Mayg2";
+import { useState } from "react";
+import { Button } from "antd";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Mayg3 from "./Mayg3";
+import Mayg4 from "./Mayg4";
+import СТ_32Б from "./СТ_32Б";
+
+function App() {
+  const [type, setType] = useState(1);
+  return (
+    <div>
+      {/* 39 iig zurj ehelj bga */}
+      <Button onClick={() => setType(1)}>Mayg1</Button>
+      <Button onClick={() => setType(2)}>Mayg2</Button>
+      <Button onClick={() => setType(3)}>Mayg3</Button>
+      <Button onClick={() => setType(4)}>Mayg4</Button>
+      {type === 1 ? <СТ_32Б /> : null}
+      {type === 2 ? <Mayg2 /> : null}
+      {type === 3 ? <Mayg3 /> : null}
+      {type === 4 ? <Mayg4 /> : null}
+    </div>
+  );
+}
+
+export default App;
