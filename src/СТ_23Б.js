7752c7a7c75cf047b548093764b6ce3ba3d44e1b
enkhturBaina <eegii.111.gg@gmail.com>
2023-04-20 12:58:44 +0800

    zaswar

--- src/СТ_23Б.js
+++ src/СТ_23Б.js
@@ -196,7 +196,224 @@ function СТ_23Б() {
           <Table bordered className="document" style={{ marginBottom: 0 }}>
             <tbody>
               <tr>
-                <td style={{ height: 30 }}></td>
+                <td colSpan={4} style={styles.centerText}>
+                  <b>Бодит үзлэгийн хэсэг:</b>
+                </td>
+              </tr>
+              <tr>
+                <td style={styles.leftText}>
+                  <div style={styles.rowStyle}>
+                    <b>Биеийн ерөнхий байдал:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Дунд</div>
+                  <div style={styles.rowStyle}>□ Хүндэвтэр</div>
+                  <div style={styles.rowStyle}>□ Хүнд</div>
+                  <div style={styles.rowStyle}>□ Маш хүнд</div>
+                  <div style={styles.rowStyle}>□ Агональ</div>
+                  <div style={styles.rowStyle}>□ Клиник үхэл</div>
+                </td>
+                <td style={styles.leftText}>
+                  <div style={styles.rowStyle}>
+                    <b>Ухаан санаа:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Саруул</div>
+                  <div style={styles.rowStyle}>□ Бүдгэрсэн</div>
+                  <div style={styles.rowStyle}>□ Дөжрөн балартах</div>
+                  <div style={styles.rowStyle}>□ Оворго</div>
+                  <div style={styles.rowStyle}>□ Бусад</div>
+                  <div style={styles.rowStyle}>________________________</div>
+                </td>
+                <td style={styles.leftText}>
+                  <div style={styles.rowStyle}>
+                    <b>Орчиндоо:</b>
+                  </div>
+                  <div style={styles.rowStyle}>
+                    □ Харьцаатай
+                    <span style={{ marginLeft: 40 }}>□ Харьцаагүй</span>
+                  </div>
+                  <div style={styles.rowStyle}>
+                    <b>Байрлал:</b>
+                  </div>
+                  <div style={styles.rowStyle}>
+                    □ Идэвхтэй<span style={{ marginLeft: 40 }}>□ Идэвхгүй</span>
+                  </div>
+                  <div style={styles.rowStyle}>□ Хагас суугаа</div>
+                  <div style={styles.rowStyle}>□ Албадмал</div>
+                  <div style={styles.rowStyle}>□ Хэвтрийн бусад __________</div>
+                </td>
+                <td style={styles.leftText}>
+                  <div style={styles.rowStyle}>
+                    <b>Хөдөлгөөн:</b>
+                  </div>
+                  <div style={styles.rowStyle}>
+                    □ Идэвхтэй<span style={{ marginLeft: 40 }}>□ Идэвхгүй</span>
+                  </div>
+
+                  <div style={styles.rowStyle}>
+                    <b>Биеийн галбир:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Хэвийн</div>
+                  <div style={styles.rowStyle}>□ Тарган</div>
+                  <div style={styles.rowStyle}>□ Туранхай</div>
+                  <div style={styles.rowStyle}>□ Туранхайвтар</div>
+                </td>
+              </tr>
+              <tr>
+                <td style={styles.leftText}>
+                  <div style={styles.rowStyle}>
+                    <b>Арьсны өнгө:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Ердийн</div>
+                  <div style={styles.rowStyle}>□ Цайвар</div>
+                  <div style={styles.rowStyle}>□ Цайвар ягаан</div>
+                  <div style={styles.rowStyle}>□ Шар</div>
+                  <div style={styles.rowStyle}>□ Саарал</div>
+                  <div style={styles.rowStyle}>□ Бусад____________</div>
+                </td>
+                <td style={styles.leftText}>
+                  <div style={styles.rowStyle}>
+                    <b>Уян чанар:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Хэвийн</div>
+                  <div style={styles.rowStyle}>□ Буурсан</div>
+                  <div style={styles.rowStyle}>□ Алдагдсан</div>
+                  <div style={styles.rowStyle}>
+                    <b>Арьсны чийглэг байдал:</b>
+                  </div>
+
+                  <div style={styles.rowStyle}>□ Хэвийн</div>
+                  <div style={styles.rowStyle}>□ Ихэссэн</div>
+                  <div style={styles.rowStyle}>□ Хуурайшсан</div>
+                </td>
+                <td style={styles.leftText}>
+                  <div style={styles.rowStyle}>
+                    <b>Тууралт:</b>
+                  </div>
+                  <div style={styles.rowStyle}>
+                    □ Цэвэр
+                    <span style={{ marginLeft: 5 }}>□ Тууралтгүй</span>
+                    <span style={{ marginLeft: 5 }}>□ Тууралттай</span>
+                  </div>
+                  <div style={styles.rowStyle}>
+                    □ Улаан
+                    <span style={{ marginLeft: 5 }}>□ Толбон</span>
+                    <span style={{ marginLeft: 5 }}>□ Гүвдрүүт</span>
+                  </div>
+                  <div style={styles.rowStyle}>
+                    □ Цэврүүт
+                    <span style={{ marginLeft: 5 }}>□ Бэлцрүүт</span>
+                    <span style={{ marginLeft: 5 }}>□ Зангилаат</span>
+                  </div>
+                  <div style={styles.rowStyle}>
+                    □ Цусархаг
+                    <span style={{ marginLeft: 5 }}>□ Хөхрөлт</span>
+                    <span style={{ marginLeft: 5 }}>□ Бусад</span>
+                  </div>
+                  <div style={styles.rowStyle}>
+                    _____________________________________
+                  </div>
+                  <div style={styles.rowStyle}>
+                    _____________________________________
+                  </div>
+                </td>
+                <td style={styles.topText}>
+                  <div style={styles.rowStyle}>
+                    Тууралтын байршил:______________
+                  </div>
+                  <div style={styles.rowStyle}>
+                    _____________________________________
+                  </div>
+                  <div style={styles.rowStyle}>
+                    _____________________________________
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td style={styles.topText}>
+                  <div style={styles.rowStyle}>
+                    <b>Захын тунгалагийн булчирхай:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Тэмтрэгдэхгүй</div>
+                  <div style={styles.rowStyle}>□ Тэмтрэгдэнэ___________</div>
+                </td>
+                <td style={styles.topText}>
+                  <div style={styles.rowStyle}>
+                    <b>Булчингийн хүчдэл:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Ердийн</div>
+                  <div style={styles.rowStyle}>□ Сул</div>
+                  <div style={styles.rowStyle}>□ Хүчдэлгүй</div>
+                  <div style={styles.rowStyle}>□ Ихэссэн</div>
+                </td>
+                <td style={styles.topText}>
+                  <div style={styles.rowStyle}>
+                    <b>Мэдрэлийн үзлэг:</b>
+                  </div>
+                  <div style={styles.rowStyle}>
+                    Дагзны хөшингө илэрсэн эсэх + -
+                  </div>
+                  <div style={styles.rowStyle}>
+                    См Керниг + - См Брузенский + -
+                  </div>
+                </td>
+                <td style={styles.topText}>
+                  <div style={styles.rowStyle}>
+                    <b>Таталт:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Үгүй</div>
+                  <div style={styles.rowStyle}>□ Хэсгийн</div>
+                  <div style={styles.rowStyle}>□ Нилэнхүйдээ</div>
+                  <div style={styles.rowStyle}>□ Шилжимхий</div>
+                  <div style={styles.rowStyle}>□ Чичирхийлсэн</div>
+                  <div style={styles.rowStyle}>□ Хүчдэлийн</div>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={4} style={styles.leftText}>
+                  <b>
+                    Толгойн
+                    хэлбэр:____________________________________________________________________Гавлын
+                    ясжилт
+                  </b>
+                  :/зур/ хэвийн, зөөлөн
+                </td>
+              </tr>
+              <tr>
+                <td style={styles.topText}>
+                  <div style={styles.rowStyle}>
+                    <b>Зулай:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Битүүрсэн</div>
+                  <div style={styles.rowStyle}>
+                    □ Битүүрээгүй /тайван, хонхор, дүүрэнгэ, лугшилттай/:
+                  </div>
+                  <div style={styles.rowStyle}>Зулай хэмжээ:__________см</div>
+                </td>
+                <td style={styles.topText}>
+                  <div style={styles.rowStyle}>
+                    <b>Хүүхэн хараа:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Хэвийн</div>
+                  <div style={styles.rowStyle}>□ Нарийн</div>
+                  <div style={styles.rowStyle}>□ Өргөссөн</div>
+                  <div style={styles.rowStyle}>□ 2 талд ижил биш</div>
+                </td>
+                <td style={styles.topText}>
+                  <div style={styles.rowStyle}>
+                    <b>Гэрлийн урвал: + -</b>
+                  </div>
+                  <div style={styles.rowStyle}>Эвэрлэгийн рефлекс: + -</div>
+                </td>
+                <td style={styles.topText}>
+                  <div style={styles.rowStyle}>
+                    <b>Чих:Өвдөлт:</b>
+                  </div>
+                  <div style={styles.rowStyle}>□ Тийм</div>
+                  <div style={styles.rowStyle}>□ Үгүй</div>
+                  <div style={styles.rowStyle}>
+                    □ См Трагус +- /баруун,зүүн/
+                  </div>
+                </td>
               </tr>
             </tbody>
           </Table>
