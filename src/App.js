458ebec7f9cfa04024817de9cec78ab80b3eeaf2
enkhturBaina <eegii.111.gg@gmail.com>
2023-04-26 18:06:04 +0800

    zasswra

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import { Button } from "antd";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CM_2_12 from "./CM/CM_2_12";
+import EIM_5_2 from "./EIM/EIM_5_2";
 
 function App() {
   // zaswar 16А zurj bga1
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <Button onClick={() => setType(1)}>Mayg1</Button>
-      {type === 1 ? <CM_2_12 /> : null}
+      {type === 1 ? <EIM_5_2 /> : null}
     </div>
   );
 }
