ea3b133539c07f5cdea5b5c2b7a3fd39271ebdd7
enkhturBaina <eegii.111.gg@gmail.com>
2023-05-02 11:28:14 +0800

    zaswar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { useState } from "react";
 import { Button } from "antd";
 import "bootstrap/dist/css/bootstrap.min.css";
-import T6_8_2 from "./Tushaal/T6_8_2";
 import T6_9 from "./Tushaal/T6_9";
 
 function App() {
